342a31017befec22a29ffb57504e56562d5827f5
raj-xe <dhananirajr@gmail.com>
2021-06-29 14:28:24 -0500

    add explanation of the iteration

--- src/main/java/day1/multidim.java
+++ src/main/java/day1/multidim.java
@@ -18,6 +18,20 @@ import java.util.Scanner;
 public class multidim {
     public static void main(String[] args) {
         int[][] a = new int[4][4];
+        /*
+        *   explore each and every floor: f -> 0 to 2
+        *       for f, we will visit each and every room: r -> 0 to 3
+        *           f,r
+        * */
+
+//        for (int i = 0; i <= 2; i++) {
+//            for (int j = 0; j <= 3; j++) {
+//                System.out.print(i+","+j+" ");
+//            }
+//            System.out.print("\n");
+//        }
+
+
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < 4; i++) { // row
             for (int j = 0; j < 4; j++) { // column
@@ -35,9 +49,19 @@ public class multidim {
 }
 
 /*
+
+{
+    1, <- 0
+    2, <- 1
+    4  <- 2
+}
+
+* */
+
+/*
 * {
-*   {1,2,3,4},
-*   {5,6,7,8},
+*   {1,2,3,4}, <- 0
+*   {5,6,7,8}, <- 1
 *   ...
 * }
 *
